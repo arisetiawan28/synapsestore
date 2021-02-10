e2f66f7422b81769c2c9b153d5b917e9d3b635b5
Ari <setiawan.ari28@gmail.com>
2021-02-11 04:27:16 +0700

    database dan tabel barang

---
+++ database/migrations/2021_02_10_100555_create_barang_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+
+            $table->string ("kodeBarang");
+            $table->string ("namaBarang");
+            $table->float ("hargaBarang");
+            $table->string ("deskripsiBarang");
+            $table->integer ("jumlahBarang");
+            
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barang');
+    }
+}
